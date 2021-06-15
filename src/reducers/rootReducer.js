664d4e46f3441f157d3477d31a18d00ef344f5e2
oscarcodefabric <oscara@codefabric.io>
2021-06-15 19:42:48 -0400

    changed title in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import { ADD_ITEM, SET_CHECK, DELETE_ITEM } from "../constants/rootConstants";
 
 const initialState = {
-  title: "React Test",
+  title: "React Test 2",
   pokemon: [],
   current_id: 0,
   isLoadingItems: false,

d1e65f5be06d142b6de7e0868effae9bbbc680d8
oscarcodefabric <oscara@codefabric.io>
2021-06-17 21:03:31 -0400

    update rootReducer
    a

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import { ADD_ITEM, SET_CHECK, DELETE_ITEM } from "../constants/rootConstants";
 
 const initialState = {
-  title: "React Test 3",
+  title: "React Test 4",
   pokemon: [],
   current_id: 0,
   isLoadingItems: false,
